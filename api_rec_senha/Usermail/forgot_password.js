e01482b947595096e3d13815c52dfcc629736f33
Nathan173Z <91638343+Nathan173Z@users.noreply.github.com>
2022-09-16 00:02:38 -0300

    Update forgot_password.js

--- api_rec_senha/Usermail/forgot_password.js
+++ api_rec_senha/Usermail/forgot_password.js
@@ -1,5 +1,5 @@
 exports.recoverypasscorp = (dados) => {
-    htmlbody += '<h1>Olá {name}!</h1>';
+    htmlbody = '<h1>Olá {name}!</h1>';
     htmlbody += '<h1>Deseja troca sua senha ? Acesso o link a baixo e valide o codigo para recuperar o acesso a sua conta</h1>'
     htmlbody += '<p><a target="_blank" href="http://127.0.0.1:5173/recoverypassword">Link para recuperar sua conta</a></p>'
     htmlbody += '<h2> Código:{token}</h2> '
