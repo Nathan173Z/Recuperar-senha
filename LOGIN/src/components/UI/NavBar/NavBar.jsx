3104cb620e7814917789e526f61c240221bcca5f
Nathan173Z <91638343+Nathan173Z@users.noreply.github.com>
2022-09-16 00:08:56 -0300

    Update NavBar.jsx

--- LOGIN/src/components/UI/NavBar/NavBar.jsx
+++ LOGIN/src/components/UI/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 
 import { Context } from '../../../Context/AuthContext'
-import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap'
+import { Navbar, Container, Form } from 'react-bootstrap'
 
 export const NavBar = () =>{
 
@@ -11,14 +11,8 @@ export const NavBar = () =>{
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                <Navbar.Brand href="/">Sistema Frontend</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/dashboard">Home</Nav.Link>
-                    <Nav.Link href="/categorias">Categorias</Nav.Link>
-                    <Nav.Link href="/products">Produtos</Nav.Link>
-                </Nav>
                 <Form className="d-flex">
-                    <Button variant="outline-warning" onClick={handleLogout}>Sair</Button>
+                  
                 </Form>                
                 </Container>
             </Navbar>  
